9a0227fe4990d2496d5e202f61e4ba50940e0e10
JuanJoseBotero <jjboterog@eafit.edu.co>
2025-02-28 08:15:43 -0500

    Parcial practico 1 terminado

--- flights/urls.py
+++ flights/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('registerPost/', views.registerPost, name='registerPost'),
+    path('list/', views.listFlights, name='list'),
+    path('statistics/', views.statistics, name='statistics'),
 ]
--- flights/views.py
+++ flights/views.py
@@ -17,3 +17,28 @@ def registerPost(request):
         Flight.objects.create(name=name, flight_type=itsNacional, price=price)
         print(name, itsNacional, price)
     return render(request, 'index.html')
+
+def listFlights(request):
+    flights = Flight.objects.all()
+    return render(request, 'list.html', {'context': flights})
+
+def statistics(request):
+    flights = Flight.objects.all()
+    promedio = 0
+    total = 0
+    nacional = 0
+    internacional = 0
+    for flight in flights:
+        total += flight.price
+        if flight.flight_type == 'Nacional':
+            nacional += 1
+        else:
+            internacional += 1
+    promedio = total / len(flights)
+    context = {
+        'promedio': promedio,
+        'nacional': nacional,
+        'internacional': internacional
+    }
+        
+    return render(request, 'statistics.html', {'context': context})
